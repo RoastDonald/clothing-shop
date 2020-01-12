cc1968fa6da2524960214e6146055ab8f3faa739
Andrey <a41789613@gmail.com>
2020-01-12 20:06:47 +0200

    menu-items updated with navigate via Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Route} from "react-router-dom";
+
+
+
+
+
+   const HatsPage = (props)=>{
+    console.log(props);
+    return (
+      <div>
+        <h1>HAT'S PAGE</h1>
+      </div>
+    )
+   }
 
 
 function App() {
   return (
-    <div>  
-      <HomePage
-
-      
-      /> 
+    <div>
+        <Route  path="/" component={HomePage}/>
+        <Route  path="/hats" component={HatsPage}/>
     </div>
   );
 }
