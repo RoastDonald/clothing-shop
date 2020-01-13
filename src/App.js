c36b5a23baca70ff0410c7e1588b7a86518c5af3
Andrey <a41789613@gmail.com>
2020-01-13 11:26:11 +0200

    header styles and link functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route} from "react-router-dom";
+import ShopPage from './pages/shoppage/shoppage.component';
+import {Route, Switch} from "react-router-dom";
+import Header from "./components/header/header.component";
 
 
-
-
-
-   const HatsPage = (props)=>{
-    console.log(props);
-    return (
-      <div>
-        <h1>HAT'S PAGE</h1>
-      </div>
-    )
-   }
 
 
 function App() {
   return (
     <div>
-        <Route  path="/" component={HomePage}/>
-        <Route  path="/hats" component={HatsPage}/>
+      <Header/>
+      <Switch>
+        <Route  exact path="/" component={HomePage}/>
+        <Route  path="/shop" component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
